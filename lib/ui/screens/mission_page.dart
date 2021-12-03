241ea08a353aee9ccf9107b5101715de80d307a7
Optimus-ged <gedeonyalakuhanda@gmail.com>
2021-12-03 13:26:25 +0200

    Item list added successfully

--- lib/ui/screens/mission_page.dart
+++ lib/ui/screens/mission_page.dart
@@ -23,34 +23,69 @@ class MissionPage extends StatelessWidget {
       //   ),
       // ),
       body: Column(
+        children: [_buildSearch(context), _buildItemList()],
+      ),
+    );
+  }
+
+  Widget _buildSearch(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: 40, bottom: 20),
+      child: Row(
         children: [
-          SizedBox(height: MediaQuery.of(context).padding.top),
-          _buildSearch()
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+              // decoration: BoxDecoration(color: Colors.purple),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: TextField(
+                  decoration:
+                      InputDecoration.collapsed(hintText: 'rechercher...'),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            child: Icon(Icons.verified),
+          )
         ],
       ),
     );
   }
 
-  Widget _buildSearch() {
-    return Row(
-      children: [
-        Expanded(
-          child: Container(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-            // decoration: BoxDecoration(color: Colors.purple),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 20),
-              child: TextField(
-                decoration: InputDecoration.collapsed(hintText: 'rechercher...'),
+  Widget _buildMissionItem() {
+    return Container(
+      margin: const EdgeInsets.only(left: 20, right: 20, bottom: 5),
+      child: Row(
+        children: [
+          Container(
+            height: 150,
+            width: 100,
+            decoration: BoxDecoration(color: Colors.amber),
+          ),
+          Expanded(
+            child: Container(
+              height: 150,
+              padding: EdgeInsets.only(bottom: 10),
+              // color: Colors.purple,
+              child: Container(
+                color: Colors.orange,
               ),
             ),
           ),
-        ),
-        Container(
-          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          child: Icon(Icons.verified),
-          
-        )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildItemList() {
+    return Column(
+      children: [
+        _buildMissionItem(),
+        _buildMissionItem(),
+        _buildMissionItem(),
       ],
     );
   }
