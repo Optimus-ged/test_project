4c3e9fe8662bce40858509468009b4c6c17dd2ec
Optimus-ged <gedeonyalakuhanda@gmail.com>
2021-12-03 14:33:54 +0200

    Errors fixed when displaying item list

--- lib/ui/screens/mission_page.dart
+++ lib/ui/screens/mission_page.dart
@@ -6,49 +6,88 @@ class MissionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // body: Center(
-      //   child: SizedBox(
-      //     height: 80,
-      //     width: 80,
-      //     child: ElevatedButton(
-      //       onPressed: () => Navigator.of(context).push(
-      //         MaterialPageRoute(
-      //           builder: (context) => MissionProcess(),
-      //         ),
-      //       ),
-      //       style:
-      //           ButtonStyle(shape: MaterialStateProperty.all(CircleBorder())),
-      //       child: Text('click'),
-      //     ),
-      //   ),
-      // ),
-      body: Column(
-        children: [_buildSearch(context), _buildItemList()],
+      body: Padding(
+        padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              _buildSearch(context),
+              _buildItemList(),
+            ],
+          ),
+        ),
       ),
     );
   }
 
   Widget _buildSearch(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(top: 40, bottom: 20),
+      padding: EdgeInsets.only(top: 20, bottom: 10),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Expanded(
             child: Container(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-              // decoration: BoxDecoration(color: Colors.purple),
+              padding: const EdgeInsets.only(bottom: 10),
               child: Padding(
                 padding: const EdgeInsets.only(left: 20),
-                child: TextField(
-                  decoration:
-                      InputDecoration.collapsed(hintText: 'rechercher...'),
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  decoration: BoxDecoration(
+                      color: Colors.blue[50],
+                      borderRadius: BorderRadius.circular(2)),
+                  child: Row(
+                    children: [
+                      Container(
+                        child: Icon(
+                          Icons.search,
+                          size: 20,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration.collapsed(
+                              hintText: 'rechercher...'),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
           Container(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-            child: Icon(Icons.verified),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Icon(
+                    Icons.notifications,
+                    color: Colors.blue,
+                  ),
+                ),
+                Positioned(
+                  top: 5,
+                  right: 0,
+                  child: Container(
+                    padding: const EdgeInsets.all(4),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Text(
+                      '2',
+                      style: TextStyle(
+                        fontSize: 8,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           )
         ],
       ),
@@ -63,15 +102,65 @@ class MissionPage extends StatelessWidget {
           Container(
             height: 150,
             width: 100,
-            decoration: BoxDecoration(color: Colors.amber),
+            decoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(2),
+                bottomLeft: Radius.circular(2),
+                bottomRight: Radius.circular(2),
+              ),
+              image: DecorationImage(
+                  image: AssetImage('assets/images/paysage.jpg'),
+                  fit: BoxFit.cover),
+            ),
           ),
           Expanded(
             child: Container(
               height: 150,
-              padding: EdgeInsets.only(bottom: 10),
+              padding: EdgeInsets.only(bottom: 5),
               // color: Colors.purple,
               child: Container(
-                color: Colors.orange,
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.blue[50],
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(2),
+                    // bottomLeft: Radius.circular(5),
+                    bottomRight: Radius.circular(2),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'ISIG Goma',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                        color: Colors.black54,
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    Text(
+                      'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+                      style: TextStyle(
+                        fontSize: 12,
+                      ),
+                    ),
+                    Spacer(),
+                    Container(
+                      padding: EdgeInsets.all(5),
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(2),
+                      ),
+                      child: Text(
+                        'visualiser',
+                        style: TextStyle(fontSize: 12, color: Colors.white),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
@@ -83,6 +172,12 @@ class MissionPage extends StatelessWidget {
   Widget _buildItemList() {
     return Column(
       children: [
+        _buildMissionItem(),
+        _buildMissionItem(),
+        _buildMissionItem(),
+        _buildMissionItem(),
+        _buildMissionItem(),
+        _buildMissionItem(),
         _buildMissionItem(),
         _buildMissionItem(),
         _buildMissionItem(),
