9f0580134b802176f3649c9a8b903abbf19cfcf3
Optimus-ged <gedeonyalakuhanda@gmail.com>
2021-12-03 13:13:24 +0200

    Search widget initialized

--- lib/ui/screens/mission_page.dart
+++ lib/ui/screens/mission_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'mission_process.dart';
 
 class MissionPage extends StatelessWidget {
   const MissionPage({Key key}) : super(key: key);
@@ -7,23 +6,52 @@ class MissionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () => Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => MissionProcess(),
-            ),
-          ),
-          style: ButtonStyle(
-            shape: MaterialStateProperty.all(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(100),
+      // body: Center(
+      //   child: SizedBox(
+      //     height: 80,
+      //     width: 80,
+      //     child: ElevatedButton(
+      //       onPressed: () => Navigator.of(context).push(
+      //         MaterialPageRoute(
+      //           builder: (context) => MissionProcess(),
+      //         ),
+      //       ),
+      //       style:
+      //           ButtonStyle(shape: MaterialStateProperty.all(CircleBorder())),
+      //       child: Text('click'),
+      //     ),
+      //   ),
+      // ),
+      body: Column(
+        children: [
+          SizedBox(height: MediaQuery.of(context).padding.top),
+          _buildSearch()
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSearch() {
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            // decoration: BoxDecoration(color: Colors.purple),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 20),
+              child: TextField(
+                decoration: InputDecoration.collapsed(hintText: 'rechercher...'),
               ),
             ),
           ),
-          child: Text('click'),
         ),
-      ),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          child: Icon(Icons.verified),
+          
+        )
+      ],
     );
   }
 }
