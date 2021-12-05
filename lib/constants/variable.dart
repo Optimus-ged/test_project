304435db9553d0ede84181cc5dcfce0870777f77
Optimus-ged <gedeonyalakuhanda@gmail.com>
2021-12-05 17:18:43 +0200

    errors fixed when creating variable for participant created

--- lib/constants/variable.dart
+++ lib/constants/variable.dart
@@ -1,6 +1,7 @@
+import 'package:ackaton_manage/models/task/participant_response.dart';
 import 'package:ackaton_manage/models/task/task_response.dart';
 
 class GlobalData {
   static List<TaskResponse> tasks = [];
-  static List<TaskResponse> participants = [];
+  static List<ParticipantResponse> participants = [];
 }
